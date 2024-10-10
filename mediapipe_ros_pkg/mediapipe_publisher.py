7f0bb667c11b0df2ce850da927f1a04920c32e00
naokinomura <naoki.nomura1221@gmail.com>
2024-10-10 16:34:25 +0000

    Add pnp

--- mediapipe_ros_pkg/mediapipe_publisher.py
+++ mediapipe_ros_pkg/mediapipe_publisher.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import cv2
 import mediapipe as mp
 import numpy as np
+from cv_bridge import CvBridge
 from mediapipe.framework.formats import landmark_pb2
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
@@ -13,7 +14,9 @@ from sensor_msgs.msg import Image
 class MediaPipePublisher(Node):
     def __init__(self):
         super().__init__("mediapipe_gesture_publisher")
-        self.publiser = self.create_publisher(Image, "/mediapipe_gesture", 10)
+        self.gesture_image_publisher = self.create_publisher(
+            Image, "/mediapipe_gesture", 10
+        )
 
         # TODO: Fix this
         mediapipe_model_path = Path(
@@ -28,18 +31,21 @@ class MediaPipePublisher(Node):
         self.options = vision.GestureRecognizerOptions(base_options=self.base_options)
         self.recognizer = vision.GestureRecognizer.create_from_options(self.options)
 
+        self.dtype = np.float32
+
+        self.bridge = CvBridge()
+
     def forward(self, image_msg):
-        image = self._ros_image_to_cv2(image_msg)
+        mp_image = mp.Image(
+            image_format=mp.ImageFormat.SRGB,
+            data=self.bridge.imgmsg_to_cv2(image_msg, "rgb8"),
+        )
 
-        image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+        recognition_result = self.recognizer.recognize(mp_image)
 
-        recognition_result = self.recognizer.recognize(image)
+        annotated_image = mp_image.numpy_view().copy()
 
-        hand_landmarks = recognition_result.hand_landmarks
-
-        image = image.numpy_view().copy()
-
-        for hand_landmarks in hand_landmarks:
+        for hand_landmarks in recognition_result.hand_landmarks:
             hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
             hand_landmarks_proto.landmark.extend(
                 [
@@ -51,11 +57,15 @@ class MediaPipePublisher(Node):
             )
 
             self.mp_drawing.draw_landmarks(
-                image,
+                annotated_image,
                 hand_landmarks_proto,
                 self.mp_hands.HAND_CONNECTIONS,
                 self.mp_drawing_styles.get_default_hand_landmarks_style(),
                 self.mp_drawing_styles.get_default_hand_connections_style(),
+            )
+
+            self.pointing_vector_estimation(
+                annotated_image, None, hand_landmarks_proto, method="pnp"
             )
 
         # TODO: Send as Topic
@@ -71,31 +81,115 @@ class MediaPipePublisher(Node):
         # except:
         #     pass
 
-        self.publiser.publish(self._cv2_to_ros_image(image))
+        self.gesture_image_publisher.publish(
+            self.bridge.cv2_to_imgmsg(annotated_image, "rgb8")
+        )
 
-    def _ros_image_to_cv2(self, image_msg):
-        if image_msg.encoding == "rgb8":
-            img = np.frombuffer(image_msg.data, np.uint8).reshape(
-                image_msg.height, image_msg.width, 3
-            )
-        elif image_msg.encoding == "bgr8":
-            img = np.frombuffer(image_msg.data, np.uint8).reshape(
-                image_msg.height, image_msg.width, 3
-            )
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        else:
-            raise ValueError(
-                "Unsupported image encoding: {}".format(image_msg.encoding)
+    # To estimate direction by using pnp method or depth
+    # When use pnp method, please use index finger tip and wrist landmarks
+    def pointing_vector_estimation(
+        self, rgb_image, depth_image, hand_landmarks_proto, method="pnp"
+    ):
+        (
+            index_finger_mcp,
+            index_finger_pip,
+            index_finger_dip,
+            index_finger_tip,
+        ) = [
+            hand_landmarks_proto.landmark[landmark_idx]
+            for landmark_idx in [
+                self.mp_hands.HandLandmark.INDEX_FINGER_MCP,
+                self.mp_hands.HandLandmark.INDEX_FINGER_PIP,
+                self.mp_hands.HandLandmark.INDEX_FINGER_DIP,
+                self.mp_hands.HandLandmark.INDEX_FINGER_TIP,
+            ]
+        ]
+
+        # Define 2D image points of the index finger mcp, pip, dip, and tip
+        image_points = np.array(
+            [
+                [
+                    index_finger_mcp.x * rgb_image.shape[1],
+                    index_finger_mcp.y * rgb_image.shape[0],
+                ],
+                [
+                    index_finger_pip.x * rgb_image.shape[1],
+                    index_finger_pip.y * rgb_image.shape[0],
+                ],
+                [
+                    index_finger_dip.x * rgb_image.shape[1],
+                    index_finger_dip.y * rgb_image.shape[0],
+                ],
+                [
+                    index_finger_tip.x * rgb_image.shape[1],
+                    index_finger_tip.y * rgb_image.shape[0],
+                ],
+            ],
+            dtype=self.dtype,
+        )
+
+        # Use PnP method for direction estimation
+        if method == "pnp":
+            index_finger_object_points = np.array(
+                [
+                    [0.0, 0.0, -90.0],
+                    [0.0, 0.0, -70.0],
+                    [0.0, 0.0, -45.0],
+                    [0.0, 0.0, 0.0],
+                ],
+                dtype=self.dtype,
             )
 
-        return img
+            # Camera internals
+            focal_length = rgb_image.shape[1]
+            center = (rgb_image.shape[1] / 2, rgb_image.shape[0] / 2)
+            camera_matrix = np.array(
+                [
+                    [focal_length, 0, center[0]],
+                    [0, focal_length, center[1]],
+                    [0, 0, 1],
+                ],
+                dtype=self.dtype,
+            )
 
-    def _cv2_to_ros_image(self, cv2_image):
-        image_msg = Image()
-        image_msg.height = cv2_image.shape[0]
-        image_msg.width = cv2_image.shape[1]
-        image_msg.encoding = "rgb8"
-        image_msg.is_bigendian = 0
-        image_msg.data = cv2_image.tobytes()
-        image_msg.step = len(image_msg.data) // image_msg.height
-        return image_msg
+            dist_coeffs = np.zeros((4, 1))  # Assuming no lens distortion
+
+            # Solve PnP
+            _, rvec, tvec = cv2.solvePnP(
+                index_finger_object_points,
+                image_points,
+                camera_matrix,
+                dist_coeffs,
+            )
+
+            start_point, _ = cv2.projectPoints(
+                np.array([0.0, 0.0, 0.0], dtype=self.dtype),
+                rvec,
+                tvec,
+                camera_matrix,
+                dist_coeffs,
+            )
+
+            end_point, _ = cv2.projectPoints(
+                np.array([0.0, 0.0, -300.0], dtype=self.dtype),
+                rvec,
+                tvec,
+                camera_matrix,
+                dist_coeffs,
+            )
+
+        # TODO
+        elif method == "depth":
+            # Use depth image for direction estimation
+            pass  # Implement depth-based direction estimation if needed
+
+        self.write_poining_vector(rgb_image, start_point, end_point)
+
+    def write_poining_vector(self, image, start_point, end_point):
+        cv2.line(
+            image,
+            (int(start_point[0][0][0]), int(start_point[0][0][1])),
+            (int(end_point[0][0][0]), int(end_point[0][0][1])),
+            (0, 0, 0),
+            3,
+        )
